814eca068553fb47f2ae0315b9d243959e4a8725
Jonas Knobloch <jonas.knobloch@t-online.de>
2019-06-17 14:48:39 +0200

    Add basic statement fetching

---
+++ script.js
@@ -0,0 +1,29 @@
+const API_ENDPOINT = "http://neverhaveiever.io/v1/statement";
+
+const PLACEHOLDER = document.querySelector("#js-ph-placeholder");
+const STATEMENT = document.querySelector("#js-ph-statement");
+
+function refreshStatement() {
+    axios
+        .get(API_ENDPOINT)
+        .then(function (response) {
+            if ("statement" in response.data) {
+                STATEMENT.innerHTML = response.data.statement;
+            } else {
+                STATEMENT.innerHTML = "Retarded API response."
+            }
+        })
+        .catch(function (error) {
+            console.log(error);
+            STATEMENT.innerHTML = "Seems like the API is dead."
+        })
+        .finally(function () {
+            PLACEHOLDER.classList.add("ph-placeholder--small");
+            STATEMENT.classList.remove("ph-statement--animated");
+        });
+}
+
+document.addEventListener("click", refreshStatement);
+document.addEventListener("keydown", refreshStatement);
+
+refreshStatement();
