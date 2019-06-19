4fce185b38c69f2d1f3100b519cdbe04ee4a73a4
Jonas Knobloch <jonas.knobloch@t-online.de>
2019-06-19 22:36:25 +0200

    Utilize online and offline events

--- script.js
+++ script.js
@@ -3,7 +3,14 @@ const API_ENDPOINT = "https://api.neverhaveiever.io/v1/statement";
 const PLACEHOLDER = document.querySelector("#js-ph-placeholder");
 const STATEMENT = document.querySelector("#js-ph-statement");
 
+let BLOCK_REFRESH = false;
+
 function refreshStatement() {
+
+    if (BLOCK_REFRESH) {
+        return;
+    }
+
     axios
         .get(API_ENDPOINT, { crossdomain: true })
         .then(function (response) {
@@ -26,6 +33,16 @@ function refreshStatement() {
 document.addEventListener("DOMContentLoaded", function() {
     document.addEventListener("click", refreshStatement);
     document.addEventListener("keydown", refreshStatement);
+
+    window.addEventListener("offline", function () {
+        BLOCK_REFRESH = true;
+        STATEMENT.innerHTML = "Your internet connection sucks."
+    });
+
+    window.addEventListener("online", function () {
+        BLOCK_REFRESH = false;
+        STATEMENT.innerHTML = "Welcome back! Tap to continue."
+    });
 
     refreshStatement();
 });
