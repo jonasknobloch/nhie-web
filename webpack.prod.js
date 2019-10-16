4aa2c7896092c334fccc271e204fda22657dfa0b
Jonas Knobloch <jonas.knobloch@t-online.de>
2019-10-16 15:38:27 +0200

    Copy CNAME on build
    * Required for gh pages deployment

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,19 @@
+'use strict';
+const path = require('path');
+
 const merge = require('webpack-merge');
 const common = require('./webpack.conf.js');
 
+const CopyPlugin = require('copy-webpack-plugin');
+
 module.exports = merge(common, {
+  plugins: [
+    new CopyPlugin([
+      {
+        from: path.resolve(__dirname, 'CNAME'),
+        to: path.resolve(__dirname, 'dist')},
+    ]),
+  ],
   module: {
     rules: [
       {
